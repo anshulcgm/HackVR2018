73ddc4da023ae3ccb1470a12dbe051a61beebc13
Clinton <clinton.reddie@live.com>
2018-09-15 16:06:19 -0700

    trying for smoother hand tracking

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+	public static PlayerCharacter Instance;
+
+	[SerializeField]
+	private float speed = 5f;
+
+	private void Awake()
+	{
+		Instance = this;
+	}
+
+	public void MoveToTarget(Vector3 pos)
+	{
+		Debug.Log("move");
+		transform.position = pos;
+	}
+}
